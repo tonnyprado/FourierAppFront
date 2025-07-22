1bb6474c6c60e51b16e8ae0152ad15d4ca353ac0
tonyprado <tonnypradog@hotmail.com>
2025-07-22 09:01:30 -0600

    chore: Frontend function and REST connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+import axios from 'axios';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
 function App() {
+  const [data, setData] = useState ([]);
+
+  const handleClick = async () => {
+    const signal = [1,0,-1,0,1,0,-1,0];//REEMPLAZAR POR VERDADERAS SEÑALES
+
+    const response = await axios.post('http://localhost:8080/api/fourier', signal);
+    const formatted = response.data.map((value, index) => ({freq: index, magnitude: value}));
+    setData(formatted);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>CALCULAR FOURIER</button>
+      <LineChart width={600} height={300} data={data}>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="freq" />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey="magnitude" stroke="#8884d8" />
+      </LineChart>
     </div>
   );
 }
